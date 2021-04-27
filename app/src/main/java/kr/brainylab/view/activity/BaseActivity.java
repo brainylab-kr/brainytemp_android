ec97ff3cda151035d21264f777a6df764eddfb8f
brainylab-kr <brainylab.kr@gmail.com>
2021-04-28 00:47:25 +0900

    V1.3

--- app/src/main/java/kr/brainylab/view/activity/BaseActivity.java
+++ app/src/main/java/kr/brainylab/view/activity/BaseActivity.java
@@ -1,34 +1,67 @@
 package kr.brainylab.view.activity;
 
 import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.WindowManager;
-import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDialog;
-import androidx.core.content.ContextCompat;
 import androidx.core.content.PermissionChecker;
-
-import kr.brainylab.R;
-import kr.brainylab.common.Common;
 
 
 public class BaseActivity extends AppCompatActivity {
 
     private AppCompatDialog loadingDlg = null;
     private static ProgressDialog _progressDlg = null;
+    private IntentFilter intentFilter;
+    private WindowManager.LayoutParams params;
+
+    BroadcastReceiver powerConnectionReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(intent.getAction().equals(Intent.ACTION_POWER_CONNECTED)) {
+                Toast.makeText(context, "충전기가 연결되었습니다", Toast.LENGTH_LONG).show();
+                params.screenBrightness = 1f;
+                getWindow().setAttributes(params);
+
+            }else if(intent.getAction().equals(Intent.ACTION_POWER_DISCONNECTED)) {
+                Toast.makeText(context, "충전기가 해제되었습니다", Toast.LENGTH_LONG).show();
+                params.screenBrightness = 0f;
+                getWindow().setAttributes(params);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        params = getWindow().getAttributes();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_POWER_CONNECTED);
+        intentFilter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+        registerReceiver(powerConnectionReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(powerConnectionReceiver);
     }
 
     @Override
